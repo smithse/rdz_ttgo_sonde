69993e380152ee94fe8aff6d5c446c69b064100a
Hansi, dl9rdz <dl9rdz@darc.de>
2024-08-22 21:48:57 +0000

    TFT fonts fix

--- RX_FSK/src/fonts/fonts.cpp
+++ RX_FSK/src/fonts/fonts.cpp
@@ -5,9 +5,9 @@
 
 #include "FreeMono12pt8b.h"
 #include "FreeMono9pt8b.h"
-#include "FreeSans12pt8b.h"
-#include "FreeSans18pt8b.h"
-#include "FreeSans9pt8b.h"
+#include "FreeSans12pt7b.h"
+#include "FreeSans18pt7b.h"
+#include "FreeSans9pt7b.h"
 #include "Picopixel.h"
 #include "Terminal11x16.h"
 
@@ -15,10 +15,12 @@ __attribute__((used)) const GFXfont * const allfonts[]={
 	(const GFXfont *)0x544E4F46,
 	&Terminal11x16Font,
 	&Terminal11x16Font,
-	&FreeSans9pt8b,
-	&FreeSans12pt8b,
-	&Picopixel,
-	&FreeSans18pt8b,
+        &Terminal11x16Font,
+        &Terminal11x16Font,
+	&FreeSans9pt7b,         // 5
+        &FreeSans12pt7b,        // 6
+        &Picopixel,             // 7
+        &FreeSans18pt7b,
 	&FreeMono9pt8b,
 	&FreeMono12pt8b,
 	0,
--- RX_FSK/version.h
+++ RX_FSK/version.h
@@ -1,4 +1,4 @@
 const char *version_name = "rdzTTGOsonde";
-const char *version_id = "dev20240817";
+const char *version_id = "dev20240822";
 const int SPIFFS_MAJOR=3;
-const int SPIFFS_MINOR=2;
+const int SPIFFS_MINOR=3;
